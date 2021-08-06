1317f1510b8315af06a265aaa7aceba976c6a0de
Farman Pirzada <farmanp.dev@gmail.com>
2021-08-06 12:26:10 -0400

    Create functionality to compare AQI scores

--- lib/main.rb
+++ lib/main.rb
@@ -1,22 +1,51 @@
 require 'net/http'
 require 'faraday'
+require 'json'
 
 module AirQualityAPI
+	GOOD_AQI = 'Good'
+	BAD_AQI = 'Bad'
+	BASE_URI = 'https://api.waqi.info/feed'
+	TOKEN = '3a0d0de300f2bfde02af82c7ec7e03109d4e5244'
+
+	# def initialize 
+	# 	@base_uri = 'https://api.waqi.info/feed'
+	# 	@token = '3a0d0de300f2bfde02af82c7ec7e03109d4e5244'
+	# end 
+
 	def self.get_request(city)
-		base_uri = 'https://api.waqi.info/feed'
-		token = '3a0d0de300f2bfde02af82c7ec7e03109d4e5244'
-		token_query = "?token=#{token}"
-		response = Faraday.get "https://api.waqi.info/feed/#{city}/#{token_query}"
+		token_query = "?token=#{TOKEN}"
+		response = Faraday.get "#{BASE_URI}/#{city}/#{token_query}"
+		transform_response = JSON.parse(response.body)
 	end 
 
-	# def request(method, url, timeout=nil)
-	# 	options = {
-	# 		base_uri: 'https://api.waqi.info/feed',
-	# 		headers: {
-	# 			'Content-Type' => 'application/json', 
-	# 		}					
-	# 	}
 
-	# 	Faraday.method url/
-	# end 
+	def self.request(method, uri, token)
+		Faraday.method "#{uri}"
+	end 	
+
+	def self.is_good_aqi?(aqi_score)
+		aqi_score.between?(0, 50) 
+	end 
+
+	def self.aqi_check(aqi_score)
+		is_good_aqi?(aqi_score) ? GOOD_AQI : BAD_AQI
+	end 
+
+	def render_details(response)
+		sleep(0.5)
+		city = "Denver"
+		sleep(0.5)
+		p "You have entered: #{city}"
+		response = AirQualityAPI.get_request(city)
+		sleep(0.5)
+		air_quality_score = response["data"]["aqi"]
+		p "Here is the information on the air quality for: #{city}"
+		p "PQI: #{air_quality_score}"
+		sleep(0.5)
+		p "Is it good air quality?"
+		sleep(0.5)
+		p AirQualityAPI.aqi_check(air_quality_score)
+	end 
 end 
+
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -6,18 +6,25 @@ describe AirQualityAPI do
 			it "receives a successful response from the API" do
 				city = 'Austin'
 				response = AirQualityAPI.get_request(city)			
-				expect(response.status).to eq 200
+				expect(response["status"]).to eq "ok"
 			end 
 			
-			it "receives a successful response with a valid city" do 
+			it "receives an AQI score with a valid city" do 
+				city = 'Austin'
+				response = AirQualityAPI.get_request(city)	
+				expect(response["data"]["aqi"].class).to be Integer
 			end 
 
 			it "does not receive a successful response with an invalid city" do 
+				city = 'Austina'
+				response = AirQualityAPI.get_request(city)	
+				expect(response["status"]).to eq "error"
 			end 
 		end 		
 
 		describe "has an invalid API token" do 
 			it "returns an error response status" do
+				# TODO: Finish test
 			end 
 		end
 	end 
