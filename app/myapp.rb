e27beb32060aa06ba4558069a80df3830192d6a0
Farman Pirzada <farman.pirz@gmail.com>
2021-08-08 15:32:00 -0400

    Add view pages and return payload in json format for react (#3)

--- app/myapp.rb
+++ app/myapp.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
-require "sinatra/reloader" if development?
+require 'sinatra/json'
+require 'sinatra/reloader' if development?
 
-require_relative "models/air_quality_index_helper"
+require_relative 'models/air_quality_index_helper'
 
 get '/' do
   erb :index
@@ -10,8 +11,13 @@ end
 get '/city_profile' do
   @city = params[:city]
   response = AirQualityIndexHelper.get_request(@city)
-  @aqi_score = response["data"]["aqi"]
+  @aqi_score = response['data']['aqi']
   @aqi_score_check = AirQualityIndexHelper.aqi_score_check(@aqi_score)
+  transformed_response = {
+    city: @city,
+    aqi_score: @aqi_score,
+    aqi_score_check: @aqi_score_check
+  }
 
-  erb :city_profile
+  json(transformed_response, encoder: :to_json, content_type: :json)
 end
