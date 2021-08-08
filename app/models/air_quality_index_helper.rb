7e17cb27e943c32fa8f0c123cc2ad263ca5e59fa
Farman Pirzada <farmanp.dev@gmail.com>
2021-08-08 15:31:18 -0400

    Add view pages and return payload in json format for react

--- app/models/air_quality_index_helper.rb
+++ app/models/air_quality_index_helper.rb
@@ -4,28 +4,42 @@ require 'json'
 require 'dotenv/load'
 
 module AirQualityIndexHelper
-  GOOD_AQI = 'Good'
-  BAD_AQI = 'Bad'
+  GOOD = 'Good'.freeze
+  MODERATE = 'Moderate'.freeze
+  UNHEALTHY_FOR_SENSITIVE_GROUPS = 'Unhealthy for Sensitive Groups'.freeze
+  UNHEALTHY = 'Unhealthy'.freeze
+  VERY_UNHEALTHY = 'Very Unhealthy'.freeze
+  HAZARDOUS = 'Hazardous'.freeze
   BASE_URI = ENV['AQI_FEED_URL']
   TOKEN = ENV['AQI_TOKEN']
 
   def self.get_request(city)
     token_query = "?token=#{TOKEN}"
     response = Faraday.get "#{BASE_URI}/#{city}/#{token_query}"
-    transform_response = JSON.parse(response.body)
+    JSON.parse(response.body)
   end
 
   def self.request(_method, uri, _token)
     Faraday.method uri.to_s
   end
 
-  def self.is_good_aqi?(aqi_score)
+  def self.aqi_score_check(aqi_score)
     # The score is between 0 and 50 because the
     # Air Quality Index defines this range as a good aqi score
-    aqi_score.between?(0, 50)
-  end
-
-  def self.aqi_score_check(aqi_score)
-    is_good_aqi?(aqi_score) ? GOOD_AQI : BAD_AQI
+    case aqi_score
+    when 0..50
+      GOOD
+    when 51..100
+      MODERATE
+    when 101..150
+      UNHEALTHY_FOR_SENSITIVE_GROUPS
+    when 201..300
+      UNHEALTHY
+    when 301..999
+      HAZARDOUS
+    else
+      "The number you provided, #{aqi_score} is not an acceptable score."
+    end
   end
 end
+
