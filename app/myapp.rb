f8377f3c9829bb3b44e81998a4b379c6c5f17463
Farman Pirzada <farmanp.dev@gmail.com>
2021-08-09 00:06:35 -0400

    ready for submission

--- app/myapp.rb
+++ app/myapp.rb
@@ -4,20 +4,22 @@ require 'sinatra/reloader' if development?
 
 require_relative 'models/air_quality_index_helper'
 
+set :allow_origin, 'http://localhost:4567/'
+set :allow_methods, 'GET,HEAD,POST'
+set :allow_headers, 'content-type,if-modified-since'
+set :expose_headers, 'location,link'
+
 get '/' do
   erb :index
 end
 
 get '/city_profile' do
+  response['Access-Control-Allow-Origin'] = '*'
+
   @city = params[:city]
   response = AirQualityIndexHelper.get_request(@city)
   @aqi_score = response['data']['aqi']
   @aqi_score_check = AirQualityIndexHelper.aqi_score_check(@aqi_score)
-  transformed_response = {
-    city: @city,
-    aqi_score: @aqi_score,
-    aqi_score_check: @aqi_score_check
-  }
 
-  json(transformed_response, encoder: :to_json, content_type: :json)
+  erb :city_profile
 end
